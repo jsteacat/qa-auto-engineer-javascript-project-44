8868f76d7e0a043fa742940117d4a464856ced47
Stepan Kormilin <ubscoder@gmail.com>
2023-11-17 13:15:25 +0300

    brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -4,39 +4,39 @@ import { wrongAnswer } from '../src/index.js'
 
 console.log('Welcome to the Brain Games!')
 
-const goEven = () => {
+const goCalc = () => {
     const name = readlineSync.question('May I have your name? ')
     console.log(`Hello, ${name}!`)
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
+    console.log('What is the result of the expression?')
 
-    console.log('Question: 15')
+    console.log('Question: 4 + 10')
     const answer1 = readlineSync.question('Your answer: ')
-    if (answer1 === 'no') {
+    if (answer1 === '14') {
         console.log('Correct!')
     } else {
-        wrongAnswer(answer1, name, 'no')
+        wrongAnswer(answer1, name, '14')
         return
     }
 
-    console.log('Question: 6')
+    console.log('Question: 25 - 11')
     const answer2 = readlineSync.question('Your answer: ')
-    if (answer2 === 'yes') {
+    if (answer2 === '14') {
         console.log('Correct!')
     } else {
-        wrongAnswer(answer2, name, 'yes')
+        wrongAnswer(answer2, name, '14')
         return
     }
 
-    console.log('Question: 7')
+    console.log('Question: 25 * 7')
     const answer3 = readlineSync.question('Your answer: ')
-    if (answer3 === 'no') {
+    if (answer3 === '175') {
         console.log('Correct!')
     } else {
-        wrongAnswer(answer3, name, 'no')
+        wrongAnswer(answer3, name, '175')
         return
     }
 
     console.log(`Congratulations, ${name}!`)
 }
 
-goEven()
+goCalc()
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export const wrongAnswer = (answer, name, correct) => {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.`)
+    console.log(`Let's try again, ${name}!`)
+}
