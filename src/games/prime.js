403cc587e9ebb4c2d667f22bad0e5424118a18d3
Stepan Kormilin <ubscoder@gmail.com>
2023-11-27 19:46:41 +0300

    fix prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,4 +1,5 @@
 import {
+  ATTEMPTS,
   checkAnswer,
   congratulations,
   createNumber,
@@ -7,14 +8,15 @@ import {
   isPrime,
 } from '../index.js';
 
-greeting();
-const name = getName();
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
 export default () => {
-  const number = createNumber();
-  console.log(`Question: ${number}`);
-  const correctAnswer = isPrime(number) ? 'yes' : 'no';
-  if (!checkAnswer(correctAnswer, name)) return;
-  congratulations(name);
+  greeting();
+  const name = getName();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  for (let i = 0; i < ATTEMPTS; i += 1) {
+    const number = createNumber();
+    console.log(`Question: ${number}`);
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+    if (!checkAnswer(correctAnswer, name)) return;
+    congratulations(name);
+  }
 };
